c621b5cd39edf4fa4839835eec8a86521b4a1db1
Stefan Gründel <sgruendel@users.noreply.github.com>
2017-04-12 19:17:47 +0200

    Year was not parsed correctly when album name contains a ")"

--- src/radio-paradise.js
+++ src/radio-paradise.js
@@ -7,6 +7,56 @@ const cheerio = require('cheerio');
 
 var exports = module.exports = {};
 
+exports.parsePlaylistBody = function(body) {
+    const $ = cheerio.load(body);
+    const entries = $('div#content table').find('tr.shade,tr.noshade').map((i, tr) => {
+        const cells = $('td', tr).map((j, td) => {
+            // For Artist - Song, parse something like
+            // <a href="rp2-content.php">Pink Floyd<br>Learning to Fly</a>
+            if ($(td).find('br').length == 1) {
+                const html = $(td).html();
+                const artistStart = html.indexOf('">') + 2;
+                const artistEnd = html.indexOf('<br>', artistStart);
+                const songStart = artistEnd + 4;
+                const songEnd = html.indexOf('</a>', songStart);
+                const artist = html.substring(artistStart, artistEnd);
+                const song = html.substring(songStart, songEnd);
+                return [artist, song];
+            }
+
+            // For Album, parse something like
+            // <a href="rp2-content.php"><img src="graphics/covers/s/B000002C1W.jpg" height="70" ...>Momentary Lapse of Reason</a> (1987)
+            if ($(td).find('img').length == 1) {
+                const html = $(td).html();
+                const coverStart = html.indexOf('<img src="') + 10;
+                const coverEnd = html.indexOf('"', coverStart);
+                const albumStart = html.indexOf('">', coverEnd) + 2;
+                const albumEnd = html.indexOf('</a>', albumStart);
+                const yearStart = html.indexOf('(', albumEnd) + 1;
+                const yearEnd = html.indexOf(')', yearStart);
+
+                const cover = html.substring(coverStart, coverEnd);
+                const album = html.substring(albumStart, albumEnd);
+                const year = html.substring(yearStart, yearEnd);
+                return [cover, album, year];
+            }
+
+            // default for PST and Rating
+            return $(td).text();
+        });
+        var entry = [];
+        entry["pst"] = cells[0];
+        entry["artist"] = cells[1];
+        entry["song"] = cells[2];
+        entry["cover"] = cells[3];
+        entry["album"] = cells[4];
+        entry["year"] = cells[5];
+        entry["rating"] = cells[6];
+        return [entry];
+    });
+    return entries;
+}
+
 exports.getPlaylist = function(callback) {
     const request = https.get({ host: 'www.radioparadise.com',
                                port: 443,
@@ -29,7 +79,7 @@ exports.getPlaylist = function(callback) {
         response.on('data', chunk => {
             body += chunk;
         });
-            
+
         response.on('end', () => {
             // we have now received the raw return data in the returnData variable.
             // We can see it in the log output via:
@@ -37,53 +87,7 @@ exports.getPlaylist = function(callback) {
             // we may need to parse through it to extract the needed data
 
             try {
-                var $ = cheerio.load(body);
-                var entries = $('div#content table').find('tr.shade,tr.noshade').map((i, tr) => {
-                    var pst, artistSong, album, rating;
-                    var cells = $('td', tr).map((j, td) => {
-                        // For Artist - Song, parse something like
-                        // <a href="rp2-content.php">Pink Floyd<br>Learning to Fly</a>
-                        if ($(td).find('br').length == 1) {
-                            const html = $(td).html();
-                            const artistStart = html.indexOf('">') + 2;
-                            const artistEnd = html.indexOf('<br>', artistStart);
-                            const songStart = artistEnd + 4;
-                            const songEnd = html.indexOf('</a>', songStart);
-                            const artist = html.substring(artistStart, artistEnd);
-                            const song = html.substring(songStart, songEnd);
-                            return [artist, song];
-                        }
-
-                        // For Album, parse something like
-                        // <a href="rp2-content.php"><img src="graphics/covers/s/B000002C1W.jpg" height="70" ...>Momentary Lapse of Reason</a> (1987)
-                        if ($(td).find('img').length == 1) {
-                            const html = $(td).html();
-                            const coverStart = html.indexOf('<img src="') + 10;
-                            const coverEnd = html.indexOf('"', coverStart);
-                            const albumStart = html.indexOf('">', coverEnd) + 2;
-                            const albumEnd = html.indexOf('</a>', albumStart);
-                            const yearStart = html.indexOf('(', albumEnd) + 1;
-                            const yearEnd = html.indexOf(')', albumStart);
-
-                            const cover = html.substring(coverStart, coverEnd);
-                            const album = html.substring(albumStart, albumEnd);
-                            const year = html.substring(yearStart, yearEnd);
-                            return [cover, album, year];
-                        }
-
-                        // default for PST and Rating
-                        return $(td).text();
-                    });
-                    var entry = [];
-                    entry["pst"] = cells[0];
-                    entry["artist"] = cells[1];
-                    entry["song"] = cells[2];
-                    entry["cover"] = cells[3];
-                    entry["album"] = cells[4];
-                    entry["year"] = cells[5];
-                    entry["rating"] = cells[6];
-                    return [entry];
-                });
+                const entries = exports.parsePlaylistBody(body);
                 return callback(null, entries);
             } catch (err) {
                 console.error('error parsing playlist', err);
--- src/test/radio-paradise.js
+++ src/test/radio-paradise.js
@@ -1,9 +1,22 @@
 'use strict';
 
-var expect = require('chai').expect;
-var radioParadise = require('../radio-paradise');
+const expect = require('chai').expect;
+const fs = require('fs');
+const radioParadise = require('../radio-paradise');
 
 describe('Radio Paradise helpers', function() {
+    describe('#parsePlaylistBody()', function() {
+        it('should parse Pink Floyd - The Dark Side Of The Moon (Immersion Box Set)', function() {
+            const body = fs.readFileSync('test/Pink_Floyd-The_Dark_Side_Of_The_Moon.html');
+            const entries = radioParadise.parsePlaylistBody(body);
+            const pinkFloydTDSOTM = entries[1];
+            expect(pinkFloydTDSOTM.artist).to.equal('Pink Floyd');
+            expect(pinkFloydTDSOTM.song).to.equal('Speak To Me -&gt; ...In The Sky (live)');
+            expect(pinkFloydTDSOTM.album).to.equal('The Dark Side Of The Moon (Immersion Box Set)');
+            expect(pinkFloydTDSOTM.year).to.equal('1974');
+        });
+    });
+
     describe('#getPlaylist()', function() {
         it('should give songs playing', function(done) {
             radioParadise.getPlaylist(function(err, result) {
