55e2f332cd2d4502e9e1d6c3fc2c7ed9ab442280
Stefan Gründel <sgruendel@users.noreply.github.com>
2017-04-12 20:01:20 +0200

    Use interpolation feature of i18next for variable replacement in i18n strings.

--- src/index.js
+++ src/index.js
@@ -15,25 +15,25 @@ const radioParadise = require('./radio-paradise');
 
 const APP_ID = 'amzn1.ask.skill.9a6c0ff8-b416-407c-be53-1c67a58fe526';
 const languageStrings = {
-    'en': {
-        translation: {
-            HELP_MESSAGE: 'You can say „Ask Paradise Playlist for current song“, or you can say „Exit“. What can I help you with?',
-            HELP_REPROMPT: 'What can I help you with?',
-            STOP_MESSAGE: 'Goodbye!',
-            CURRENTLY_PLAYING_MESSAGE: 'You\'re listening to {song} by {artist} from the {year} album {album}.',
-            AVERAGE_RATING_MESSAGE: 'Average rating by your fellow Radio Paradise listeners is {rating}.',
-            CANT_GET_PLAYLIST_MESSAGE: 'I\'m sorry, I can\'t get that information currently.',
+    "en": {
+        "translation": {
+            "HELP_MESSAGE": "You can say „Ask Paradise Playlist for current song“, or you can say „Exit“. What can I help you with?",
+            "HELP_REPROMPT": "What can I help you with?",
+            "STOP_MESSAGE": "Goodbye!",
+            "CURRENTLY_PLAYING_MESSAGE": "You're listening to {{song}} by {{artist}} from the {{year}} album {{album}}.",
+            "AVERAGE_RATING_MESSAGE": "Average rating by your fellow Radio Paradise listeners is {{rating}}.",
+            "CANT_GET_PLAYLIST_MESSAGE": "I'm sorry, I can't get that information currently.",
         },
     },
         
-    'de': {
-        translation: {
-            HELP_MESSAGE: 'Du kannst sagen „Frage Paradise Playlist nach dem aktuellen Lied“, oder du kannst „Beenden“ sagen. Wie kann ich dir helfen?',
-            HELP_REPROMPT: 'Wie kann ich dir helfen?',
-            STOP_MESSAGE: 'Auf Wiedersehen!',
-            CURRENTLY_PLAYING_MESSAGE: 'Du hörst gerade {song} von {artist} aus dem Album {album} von {year}.',
-            AVERAGE_RATING_MESSAGE: 'Die durchschnittliche Bewertung aller Radio Paradise-Hörer ist {rating}.',
-            CANT_GET_PLAYLIST_MESSAGE: 'Es tut mir leid, das kann ich gerade nicht herausfinden.',
+    "de": {
+        "translation": {
+            "HELP_MESSAGE": "Du kannst sagen „Frage Paradise Playlist nach dem aktuellen Lied“, oder du kannst „Beenden“ sagen. Wie kann ich dir helfen?",
+            "HELP_REPROMPT": "Wie kann ich dir helfen?",
+            "STOP_MESSAGE": "Auf Wiedersehen!",
+            "CURRENTLY_PLAYING_MESSAGE": "Du hörst gerade {{song}} von {{artist}} aus dem Album {{album}} von {{year}}.",
+            "AVERAGE_RATING_MESSAGE": "Die durchschnittliche Bewertung aller Radio Paradise-Hörer ist {{rating}}.",
+            "CANT_GET_PLAYLIST_MESSAGE": "Es tut mir leid, das kann ich gerade nicht herausfinden.",
         },
     },
 };
@@ -49,14 +49,11 @@ const handlers = {
         radioParadise.getPlaylist((err, playlist) => {
             if (playlist && playlist[0]) {
                 var entry = playlist[0];
-                const nowPlaying = this.t('CURRENTLY_PLAYING_MESSAGE')
-                      .replace('{artist}', entry.artist)
-                      .replace('{song}', entry.song)
-                      .replace('{album}', entry.album)
-                      .replace('{year}', entry.year);
+                const nowPlaying = this.t('CURRENTLY_PLAYING_MESSAGE',
+                                          { artist: entry.artist, song: entry.song, album: entry.album, year: entry.year });
                 const speechOutput = nowPlaying;
                 const cardContent = nowPlaying
-                      + ' ' + this.t('AVERAGE_RATING_MESSAGE').replace('{rating}', entry.rating);
+                      + ' ' + this.t('AVERAGE_RATING_MESSAGE', { rating: entry.rating });
 
                 // TODO Can't use imageObj due to cross-origin resource sharing (CORS) restrictions, see
                 // https://developer.amazon.com/blogs/post/Tx15T24P9QV8RXN/New-ASK-Features-Standard-Home-Cards-with-Image-Support
@@ -64,6 +61,7 @@ const handlers = {
                     smallImageUrl: entry.cover,
                     largeImageUrl: entry.cover.replace('\/s\/', '/l/')
                 }
+                console.log(cardContent);
 
                 this.emit(':tellWithCard', speechOutput, 'Radio Paradise Playlist', cardContent);
             } else {
